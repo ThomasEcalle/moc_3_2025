20e2e5e43553197a4175500b8c29a088c8ae1332
ThomasEcalle <thomasecalle@gmail.com>
2025-04-15 17:04:04 +0200

    Add first stateful example

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'home_tabs/home_tab.dart';
+import 'home_tabs/settings_tab.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _tabs = [
+    HomeTab(),
+    SettingsTab(),
+  ];
+
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          'OK',
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coucou'),
+        backgroundColor: _currentIndex == 0 ? Colors.blue : Colors.purple,
+        centerTitle: true,
+        leading: Icon(Icons.add),
+        actions: [
+          Icon(Icons.add),
+          Icon(Icons.abc),
+        ],
+      ),
+      body: _tabs[_currentIndex],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _currentIndex == 0 ? Colors.blue : Colors.red,
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            _currentIndex = 0;
+          });
+        }
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+          print('New currentIndex = $_currentIndex');
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.add),
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
       ),
     );
   }
